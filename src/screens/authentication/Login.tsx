d267f50027e9b02f1b7979c02f6430927ae3e523
adityadeshpande0 <adityadeshpande1@outlook.com>
2025-04-04 00:42:37 +0530

    changes

--- src/screens/authentication/Login.tsx
+++ src/screens/authentication/Login.tsx
@@ -9,18 +9,7 @@ const Login: React.FC = () => {
 
   return (
     <div className="login-container">
-      <h1>Login</h1>
-      <form onSubmit={handleLogin}>
-        <div>
-          <label htmlFor="email">Email:</label>
-          <input type="email" id="email" name="email" required />
-        </div>
-        <div>
-          <label htmlFor="password">Password:</label>
-          <input type="password" id="password" name="password" required />
-        </div>
-        <button type="submit">Login</button>
-      </form>
+     
     </div>
   );
 };
