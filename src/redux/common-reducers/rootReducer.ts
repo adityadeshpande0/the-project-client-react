72a2a1fa497455f467fd77e13a38167fcac16348
adityadeshpande0 <adityadeshpande1@outlook.com>
2025-05-06 16:09:45 +0530

    redux setup

---
+++ src/redux/common-reducers/rootReducer.ts
@@ -0,0 +1,12 @@
+import { combineReducers } from '@reduxjs/toolkit';
+// Import your slices here
+// import counterReducer from '../slices/counterSlice';
+// import userReducer from '../slices/userSlice';
+
+const rootReducer = combineReducers({
+  // Add your reducers here
+  // counter: counterReducer,
+  // user: userReducer,
+});
+
+export default rootReducer;
