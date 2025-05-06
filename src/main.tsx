c2d124eb3296ecf2befd2ffa95310fa667d4f414
adityadeshpande0 <adityadeshpande1@outlook.com>
2025-05-06 16:16:09 +0530

    react query setup

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
-import { Provider } from 'react-redux'
-import store from './redux/store.ts'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { Provider } from "react-redux";
+import store from "./redux/store.ts";
 // import "bootstrap/dist/css/bootstrap.min.css";
-createRoot(document.getElementById('root')!).render(
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+const queryClient = new QueryClient();
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <Provider store={store}>
-    <App />
-    </Provider>
-  </StrictMode>,
-)
+    <QueryClientProvider client={queryClient}>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </QueryClientProvider>
+  </StrictMode>
+);
