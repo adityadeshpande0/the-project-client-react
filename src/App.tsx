aa898d1ee877ca14536b86658e149ac8b7eae170
adityadeshpande0 <adityadeshpande1@outlook.com>
2025-05-06 23:12:40 +0530

    app changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
 import AppRouter from "./routing/AppRouter";
-import Login from "./screens/authentication/Login/Login";
-import QuestionsScreen from "./screens/questions/QuestionsScreen";
 // import "bootstrap/dist/css/bootstrap.min.css";
 function App() {
   return (
