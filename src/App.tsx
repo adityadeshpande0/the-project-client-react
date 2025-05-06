98490503dfb5fa5e22f2c1e16f9f3c13a786e6bd
adityadeshpande0 <adityadeshpande1@outlook.com>
2025-05-06 18:50:21 +0530

    router setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
+import AppRouter from "./routing/AppRouter";
 import Login from "./screens/authentication/Login/Login";
 import QuestionsScreen from "./screens/questions/QuestionsScreen";
 // import "bootstrap/dist/css/bootstrap.min.css";
 function App() {
   return (
-    <>
-      {/* <Login /> */}
-      <QuestionsScreen />
-    </>
+   <AppRouter/>
   );
 }
 
--- src/routing/AppRouter.tsx
+++ src/routing/AppRouter.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Login from "../screens/authentication/Login/Login";
+import QuestionsScreen from "../screens/questions/QuestionsScreen";
 
 const AppRouter: React.FC = () => {
   return (
     <Router>
       <Routes>
         <Route path="/login" Component={Login} />
+        <Route path="/questions" Component={QuestionsScreen} />
       </Routes>
     </Router>
   );
