32e5d15c99887d7682203528dbe32306f601706e
adityadeshpande0 <adityadeshpande1@outlook.com>
2025-05-06 23:10:05 +0530

    login screen ui improved

--- src/screens/authentication/Login/Login.tsx
+++ src/screens/authentication/Login/Login.tsx
@@ -1,11 +1,21 @@
 import React from "react";
 import LoginForm from "../LoginForm";
-
+import login_image from "../../../assets/login_image.png";
+import "./loginStyles.scss";
 
 const Login: React.FC = () => {
- return(
-    <LoginForm/>
- )
+  return (
+    <div className="login-screen-main-container">
+      <LoginForm />
+      <div className="login-screen-image-container">
+        <img
+          style={{ width: "100%", objectFit: "fill", height: "100%" }}
+          src={login_image}
+          alt="login_image"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Login;
